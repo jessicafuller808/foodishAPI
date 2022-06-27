74b1eb2f027f34e159b1286f8cba01031c1b5247
Jessica Fuller <jessicanfuller808@gmail.com>
2022-06-27 16:52:25 -0700

    setup an event listener on button that fetches the foodish api, then relays the image data as a source for the empty image element.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 //Foodish API Docs - https://github.com/surhud004/Foodish#readme
 //------------------------------------------------
-//Sends request to the site
+
+//Targets the button element and listens for a click. When clicked, getMeal is run.
+document.querySelector('button').addEventListener('click', getMeal);
+
+
+function getMeal() {
+  //Sends request to the site
 fetch("https://foodish-api.herokuapp.com/api")
 .then(res => res.json()) // parses response as JSON
 .then(data => { // passes the JSON as data
   console.log(data);
+
+  document.querySelector('img').src = data.image;
 })
   
 .catch(err => {
     console.log(`${err}`)
 });
+
+}
+
 
